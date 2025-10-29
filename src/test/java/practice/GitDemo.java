1f6ec2f48614f32c97e2d86ac609c38da7aa8e30
Mohana <Mohana@DESKTOP-FFIO7TC>
2025-10-29 11:57:18 +0530

    updated changes

---
+++ src/test/java/practice/GitDemo.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class GitDemo {
+@Test
+private void syso() {
+	// TODO Auto-generated method stub
+System.out.println("new changes");
+}
+}
